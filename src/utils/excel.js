498f590abbe15c02043d2a923095f40f48ec1ab6
Ashmzp <ash.mzp001@gmail.com>
2025-11-19 14:00:57 +0530

    Fix: Remove HTML encoding from Excel export (M/S showing as M&#x2F;S)

--- src/utils/excel.js
+++ src/utils/excel.js
@@ -1,21 +1,11 @@
 import * as XLSX from 'xlsx';
-import { escapeHTML } from './sanitize';
 
 export const exportToExcel = (data, filename) => {
   try {
     if (!data || data.length === 0) {
       throw new Error('No data to export');
     }
-    // Sanitize data before export
-    const sanitizedData = data.map(row => {
-      const sanitizedRow = {};
-      Object.keys(row).forEach(key => {
-        const value = row[key];
-        sanitizedRow[key] = typeof value === 'string' ? escapeHTML(value) : value;
-      });
-      return sanitizedRow;
-    });
-    const worksheet = XLSX.utils.json_to_sheet(sanitizedData);
+    const worksheet = XLSX.utils.json_to_sheet(data);
     const workbook = XLSX.utils.book_new();
     XLSX.utils.book_append_sheet(workbook, worksheet, 'Sheet1');
     XLSX.writeFile(workbook, `${filename}.xlsx`);
